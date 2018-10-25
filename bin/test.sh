85c0fd5099c6810ac7448822399b55b61e4823ad
BCC <bcc@gmail.com>
2018-10-25 11:53:12 -0400

    2

--- bin/test.sh
+++ bin/test.sh
@@ -2,10 +2,10 @@
  
 set -e
  
-ganache-cli  2> /dev/null 1> /dev/null &
-sleep 5 # to make sure ganache-cli is up and running before compiling
-rm -rf build
-truffle compile
-truffle migrate --reset --network development
-truffle test
-kill -9 $(lsof -t -i:8545)
+#ganache-cli  2> /dev/null 1> /dev/null &
+#sleep 5 # to make sure ganache-cli is up and running before compiling
+#rm -rf build
+#truffle compile
+#truffle migrate --reset --network development
+#truffle test
+#kill -9 $(lsof -t -i:8545)
