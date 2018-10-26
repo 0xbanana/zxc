529baa7f8767ecf55c93029c8236c5ee19c1ea1e
BCC <bcc@gmail.com>
2018-10-25 21:09:15 -0400

    RUNAWAY

--- looper.sh
+++ looper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ls contracts/ | while read a; do (echo "[FILE]: $a"; docker run -v $(pwd):/tmp luongnguyen/oyente oyente/oyente.py -s /tmp/contracts/$a -ce > ./output/oyente_$a); done;
+ls contracts/ | while read a; do (echo "[FILE]: $a"; docker run -v $(pwd):/tmp luongnguyen/oyente oyente/oyente.py -s /tmp/contracts/$a -ce ); done;
