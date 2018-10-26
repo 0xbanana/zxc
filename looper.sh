d1b3ccf5a91cc8cd27e77c1f3a4d8edf4fe6ff85
BCC <bcc@gmail.com>
2018-10-25 21:15:53 -0400

    EEEEEEK

--- looper.sh
+++ looper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ls contracts/ | while read a; do (echo "[FILE]: $a"; docker run -v $(pwd):/tmp luongnguyen/oyente oyente/oyente.py -s /tmp/contracts/$a -ce ); done;
+ls contracts/*.sol | while read a; do (echo "[FILE]: $a"; docker run -v $(pwd):/tmp luongnguyen/oyente oyente/oyente.py -s /tmp/$a -ce ); done;
