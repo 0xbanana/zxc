e69bebc40dd91a12e6f7888e4b1a98d87c4a0d89
BCC <bcc@gmail.com>
2018-10-26 12:34:10 -0400

    DUMBO

--- static.sh
+++ static.sh
@@ -12,11 +12,11 @@ ls contracts/*.sol |
 		echo "[Found File]: $a";
 		echo "";
 		echo "Running task oyente"; 
-		docker run -v $(pwd):/tmp luongnguyen/oyente oyente/oyente.py -s /tmp/$a -ce > $a_boop;
+		docker run -v $(pwd):/tmp luongnguyen/oyente oyente/oyente.py -s /tmp/$a -ce > youreanidiot.txt;
 
 		echo "--------------------------------------------------------------------"
 		echo "\n\nRunning task echidna-test\n"
-		docker run -v $(pwd):/tmp trailofbits/echidna echidna-test /tmp/$a;
+		docker run -v $(pwd):/tmp trailofbits/echidna echidna-test /tmp/$a > youreanidiot.txt;
 
 
 	); done ;
