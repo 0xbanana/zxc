2c842cb8f934ae1ad7afcbcd9fa6f14677d6c4b5
BCC <bcc@gmail.com>
2018-10-26 11:49:23 -0400

    MEAT & CHEESE

--- static.sh
+++ static.sh
@@ -3,12 +3,14 @@
 # Static analyzers for smart contracts 
 
 echo "[Static Tasks]";
-echo "Getting all contracts in ./contracts/\n";
+echo "Getting all contracts in ./contracts/";
+echo "";
 
 ls contracts/*.sol | 
 	while read a; 
 	do (
-		echo "[Found File]: $a\n";
+		echo "[Found File]: $a";
+		echo "";
 		echo "Running task oyente"; 
 		docker run -v $(pwd):/tmp luongnguyen/oyente oyente/oyente.py -s /tmp/$a -ce;
 
@@ -16,5 +18,6 @@ ls contracts/*.sol |
 		echo "\n\nRunning task echidna-test\n"
 		docker run -v $(pwd):/tmp trailofbits/echidna echidna-test /tmp/$a;
 
-		echo "========ALL STATIC TASKS DONE================="
+
 	); done ;
+echo "========ALL STATIC TASKS DONE================="
