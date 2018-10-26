74e1cc0c7c3c37c7a3df2f3b4f5743d7c3ecf703
BCC <bcc@gmail.com>
2018-10-25 21:35:49 -0400

    can i be a real boy?

--- looper.sh
+++ looper.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-ls contracts/*.sol | while read a; do (echo "[FILE]: $a"; docker run -v $(pwd):/tmp luongnguyen/oyente oyente/oyente.py -s /tmp/$a -ce ); done;
+ls contracts/*.sol | 
+	while read a; 
+	do (
+		echo "[FILE]: $a"; 
+		docker run -v $(pwd):/tmp luongnguyen/oyente oyente/oyente.py -s /tmp/$a -ce;
+		docker run -v $(pwd):/tmp trailofbits/echidna echidna-test /tmp/$a;
+	); done;
